ced565866fd833692e5cccd4aee2964c1abbcdf3
ashu01 <as01hu@gmail.com>
2016-06-20 15:56:03 +0530

    Error introduced

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,16 @@ fn main() {
     io::stdin().read_line(&mut guess)
     .ok()
     .expect("failed to read line");
-    
+
+    let guess: u32 = guess.trim().parse()
+    .ok()
+    .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
