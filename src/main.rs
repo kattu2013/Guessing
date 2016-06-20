b31051a26b49986136934529fa0c8b9f9de04f36
ashu01 <as01hu@gmail.com>
2016-06-20 16:15:16 +0530

    main

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ fn main() {
     println!("Guess the number!");
     
     let secret_number = rand::thread_rng().gen_range(1, 100001);
-    
-    
+       
     loop {
         println!("Please input your guess.");
     
